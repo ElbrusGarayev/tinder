22aca5de37d9e38c51a2b9ab39f73921c2e3f9fb
ElbrusGarayev <60807221+ElbrusGarayev@users.noreply.github.com>
2020-05-27 19:07:23 +0400

    DB credentials removed

--- src/main/java/database/DBConnection.java
+++ src/main/java/database/DBConnection.java
@@ -1,5 +1,6 @@
 package database;
 
+import heroku.HerokuEnv;
 import lombok.SneakyThrows;
 
 import java.sql.Connection;
@@ -13,9 +14,9 @@ public class DBConnection {
         if (conn == null) {
             try{
                 conn = DriverManager.getConnection(
-                        "jdbc:postgresql://ec2-34-197-188-147.compute-1.amazonaws.com:5432/deo8n2r3hvuml8",
-                        "vfqqrouexuyann",
-                        "2d557a8aab585f428b1f6b2e0283fa11ed22451e77f633982a79da05cff7c44d"
+                        HerokuEnv.jdbc_url(),
+                        HerokuEnv.jdbc_username(),
+                        HerokuEnv.jdbc_password()
                 );
             }catch (SQLException e){
                 e.printStackTrace();
